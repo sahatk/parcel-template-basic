87a5a653c74a32fe32a282354fccbdc360e85155
park sang jin <sahatk@naver.com>
2024-03-11 16:23:37 +0900

    create project

---
+++ .postcssrc.js
@@ -0,0 +1,16 @@
+// ESM
+// CommmonJS
+
+//import autoprefixer from 'autoprefixer'
+// const autoprefixer = require('autoprefixer')
+
+//export {
+//     plugins: [
+//         autoprefixer
+//     ]
+// }
+module.exports = {
+    plugins: [
+        require('autoprefixer')
+    ]
+}
